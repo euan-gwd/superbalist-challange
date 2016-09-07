ea5a6aab503a968a597dd482f47cc80b155f2ec0
Euan_G <euan@amwic.co.za>
2016-09-07 14:28:33 +0200

    Working with img elements

--- build/js/myKeyboardNavigatorPlugin.js
+++ build/js/myKeyboardNavigatorPlugin.js
@@ -21,20 +21,30 @@
 				console.log(event.which + "pressed");
 				fcount++;
 				console.log("keypressed " + fcount + " times");
-				// if($(element.selector).length) {
 				targetElem = $(element.selector + ":nth-of-type(" + fcount + ")");
-				$("html, body").stop().animate({
-					scrollTop: targetElem.offset().top
-				}, 1000);
+				// targetElem = selectedElem.eq(fcount);
+				if (fcount < eslength + 1) {
+					$("html, body").stop().animate({
+						scrollTop: targetElem.offset().top
+					}, 1000);
+				} else {
+					fcount = 0;
+				}
 			} //end Forward
 
 			if (e.which === 74) {
 				console.log(event.which + "pressed");
 				bcount--;
+				console.log("keypressed " + bcount + " times");
 				targetElem = $(element.selector + ":nth-of-type(" + bcount + ")");
-				$("html, body").stop().animate({
-					scrollTop: targetElem.offset().top
-				}, 1000);
+				// targetElem = selectedElem.eq(bcount);
+				if (bcount > 0) {
+					$("html, body").stop().animate({
+						scrollTop: targetElem.offset().top
+					}, 1000);
+				} else {
+					bcount = eslength + 1;
+				}
 			} //end Backward
 		}); //end keyup
 	}; //end myKeyboardNavigatorPlugin
--- src/js/myKeyboardNavigatorPlugin.js
+++ src/js/myKeyboardNavigatorPlugin.js
@@ -10,7 +10,6 @@
 		let bcount = $(element.selector).length;
 
 		$(document).keyup(function(e) {
-			// let selectedElem = $(element.selector);
 			let targetElem;
 			let eslength = $(element.selector).length;
 
@@ -20,19 +19,27 @@
 				fcount++;
 				console.log(`keypressed ${fcount} times`);
 				targetElem = $(`${element.selector}:nth-of-type(${fcount})`);
-				$("html, body").stop().animate({
-					scrollTop: targetElem.offset().top
-				}, 1000);
+				if(fcount < eslength + 1) {
+					$("html, body").stop().animate({
+						scrollTop: targetElem.offset().top
+					}, 1000);
+				} else {
+					fcount = 0;
+				}
 			} //end Forward
 
 			if(e.which === 74) {
 				console.log(event.which + "pressed");
 				bcount--;
+				console.log(`keypressed ${bcount} times`);
 				targetElem = $(`${element.selector}:nth-of-type(${bcount})`);
-				$("html, body").stop().animate({
-					scrollTop: targetElem.offset().top
-				}, 1000);
-
+				if(bcount > 0) {
+					$("html, body").stop().animate({
+						scrollTop: targetElem.offset().top
+					}, 1000);
+				} else {
+					bcount = eslength + 1;
+				}
 			} //end Backward
 
 		}); //end keyup
