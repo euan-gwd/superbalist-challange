2248b55ebd31d6a490e7377c47cb6ba57bf4bcd3
Euan_G <euan@amwic.co.za>
2016-09-13 15:33:16 +0200

    changed to reflect best practices

---
+++ build/js/app.js
@@ -0,0 +1,12 @@
+"use strict";
+
+(function () {
+	"use strict";
+
+	$(function () {
+		var options = {
+			selector: "img"
+		};
+		$(document).myKeyboardNavigatorPlugin(options);
+	});
+})();
--- build/js/myKeyboardNavigatorPlugin.js
+++ build/js/myKeyboardNavigatorPlugin.js
@@ -1,35 +1,37 @@
-(function($) {
-    "use strict";
-    $.fn.myKeyboardNavigatorPlugin = function(options) {
+"use strict";
 
-        let element = $.extend({
+(function ($) {
+    "use strict";
+
+    $.fn.myKeyboardNavigatorPlugin = function (options) {
+
+        var element = $.extend({
             selector: "img"
         }, options);
 
-        let fcount = 0;
-        let bcount = $(element.selector).length;
+        var fcount = 0;
+        var bcount = $(element.selector).length;
 
-        $(document).on('keyup',function(e) {
+        $(document).on('keyup', function (e) {
             e.stopPropagation();
-            let targetElem;
-            const eslength = $(element.selector).length + 1;
+            var targetElem = void 0;
+            var eslength = $(element.selector).length + 1;
 
             if (e.which === 75 || e.which === 39) {
                 fcount++;
-                targetElem = $(`${element.selector}:nth-of-type(${fcount})`);
+                targetElem = $(element.selector + ":nth-of-type(" + fcount + ")");
                 if (fcount < eslength) {
                     $("html, body").stop().animate({
                         scrollTop: targetElem.offset().top
                     }, 1000);
                 } else {
                     fcount = 0;
-
                 }
             } //end Forward
 
             if (e.which === 74 || e.which === 37) {
                 bcount--;
-                targetElem = $(`${element.selector}:nth-of-type(${bcount})`);
+                targetElem = $(element.selector + ":nth-of-type(" + bcount + ")");
                 if (bcount > 0) {
                     $("html, body").stop().animate({
                         scrollTop: targetElem.offset().top
@@ -38,8 +40,6 @@
                     bcount = eslength;
                 }
             } //end Backward
-
         }); //end keyup
     }; //end myKeyboardNavigatorPlugin
-
 })(jQuery);
